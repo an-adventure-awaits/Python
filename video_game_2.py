7c1c911731b1c3ea0e374620c0ae8df6658bb106
an-adventure-awaits <amcintire12345@gmail.com>
2024-10-16 14:45:19 -0600

    Update video_game_2.py

--- video_game_2.py
+++ video_game_2.py
@@ -7,7 +7,7 @@ Purpose:
 
 class video_game:
 
-    def __init__(self, _title, _price):
+    def __init__(self, _title, _price, _game_name_1, _game_name_2, _game_name_3):
 
         # _title
         self._title = _title
@@ -15,15 +15,29 @@ class video_game:
         # _price
         self._price = _price
 
-        # 
+        # game_name_1
+        self._game_name_1 = _game_name_1
+        _game_name_1 = "Minecraft"
+        self._game_name_cost_1 = 19.99
+
+        # game_name_2
+        self._game_name_2 = _game_name_2
+        _game_name_2 = "Genshin Impact"
+        self._game_name_cost_2 = 25.47
+
+        # game_name_3
+        self._game_name_3 = _game_name_3
+        _game_name_3 = "League of Legends"
+        self._game_name_cost_3 = 30.99
+
+        
 
     # display_game()
-    def display_game():
+    def display_game(self):
         
-        print(f"game 1\n")
+        print(f"The first game is {self._game_name_1} for ${self.game_name_cost_1}\n")
 
-        print(f"game 2\n")
+        print(f"The seccond game is {self._game_name_2} for ${self.game_name_cost_2}\n")
 
-        print(f"game 3")
+        print(f"The third game is {self._game_name_3} for ${self.game_name_cost_2}")
 
-        
