83c063925a0a3bda43451404ce016eceec1bb247
an-adventure-awaits <amcintire12345@gmail.com>
2024-10-16 14:24:01 -0600

    video_game.py needs to call variables

---
+++ video_game.py
@@ -0,0 +1,29 @@
+"""
+Name:video_game.py
+Author: Ansonia McIntire
+Date: October 16, 2024
+Purpose: 
+"""
+
+class video_game:
+
+    def __init__(self, _title, _price):
+
+        # _title
+        self._title = _title
+
+        # _price
+        self._price = _price
+
+        # 
+
+    # display_game()
+    def display_game():
+        
+        print(f"game 1\n")
+
+        print(f"game 2\n")
+
+        print(f"game 3")
+
+        
