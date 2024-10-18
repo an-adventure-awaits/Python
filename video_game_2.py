5a332475290ae6c3fd7b5282f397c9e4fe2c998a
an-adventure-awaits <amcintire12345@gmail.com>
2024-10-18 16:35:17 -0600

    fixed and working

--- video_game_2.py
+++ video_game_2.py
@@ -5,9 +5,9 @@ Date: October 16, 2024
 Purpose: 
 """
 
-class video_game:
+class Video_Game_Store:
 
-    def __init__(self, _title, _price, _game_name_1, _game_name_2, _game_name_3):
+    def __init__(self, _title, _price):
 
         # _title
         self._title = _title
@@ -15,29 +15,22 @@ class video_game:
         # _price
         self._price = _price
 
-        # game_name_1
-        self._game_name_1 = _game_name_1
-        _game_name_1 = "Minecraft"
-        self._game_name_cost_1 = 19.99
-
-        # game_name_2
-        self._game_name_2 = _game_name_2
-        _game_name_2 = "Genshin Impact"
-        self._game_name_cost_2 = 25.47
-
-        # game_name_3
-        self._game_name_3 = _game_name_3
-        _game_name_3 = "League of Legends"
-        self._game_name_cost_3 = 30.99
-
         
 
     # display_game()
     def display_game(self):
         
-        print(f"The first game is {self._game_name_1} for ${self.game_name_cost_1}\n")
+        print(f"The game is {self._title} and the price is {self._price}\n")
 
-        print(f"The seccond game is {self._game_name_2} for ${self.game_name_cost_2}\n")
+       
+        
 
-        print(f"The third game is {self._game_name_3} for ${self.game_name_cost_2}")
+MineCraft = Video_Game_Store("MineCraft", 19.99)
+MineCraft.display_game()
+
+Genshin_Impact = Video_Game_Store("Genshin Impact", 25.47)
+Genshin_Impact.display_game()
+
+League_of_Legends = Video_Game_Store("League of Legends", 30.99)
+League_of_Legends.display_game()
 
